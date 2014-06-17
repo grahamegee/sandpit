a519e7101973ef0d874f37b57062938f13c300c4
Grahame Gardiner <grahamegee@gmail.com>
2014-06-17 23:01:40 +0100

    simplified recursive solver and changed solve function so that it marks the successful path once the maze is solved

--- Maze_Solver/maze_solver_2.py
+++ Maze_Solver/maze_solver_2.py
@@ -2,41 +2,22 @@
 
 import argparse
 
-route   = '.' 
+#----------------------------------------------------------------------------------
+# maze chars
+#----------------------------------------------------------------------------------
+
+been    = '.' 
 new     = ' '
-deadend = '*'
+route   = '*'
 start   = 'S'
 end     = 'G'
-edge    = '\n'
 
 #----------------------------------------------------------------------------------
 
-# use these to be verbose
-def up(position):
-	print "moving up"
- 	row,col = position
-	return (row-1,col)
-
-def down(position):
-	print "moving down"
- 	row,col = position
-	return (row+1,col)
-
-
-def left(position):
-	print "moving left"
- 	row,col = position
-	return (row,col-1)
-
-def right(position):
-	print "moving right"
- 	row,col = position
-	return (row,col+1)
-
-#up    = lambda (row,col): (row-1,col)	
-#down  = lambda (row,col): (row+1,col)
-#right = lambda (row,col): (row,col+1)
-#left  = lambda (row,col): (row,col-1)
+up    = lambda (row,col): (row-1,col)	
+down  = lambda (row,col): (row+1,col)
+right = lambda (row,col): (row,col+1)
+left  = lambda (row,col): (row,col-1)
 
 #----------------------------------------------------------------------------------
 
@@ -55,74 +36,48 @@ def parse_maze(mazeFile):
 def find_start(maze):
 	""" function to extract the start position from the maze.
 	"""
-	
-	for key in maze.keys():
-		if maze[key] == start:
-			return key
+	# first index of singleton list	
+	return [key for key, val in maze.items() if val == start][0]
 
 #----------------------------------------------------------------------------------
 
 def show_maze(maze):
 	""" Print the maze to standard out
 	"""
-	positions = maze.keys()
-	positions.sort()
+	positions = maze.keys().sort()
 	print "".join([maze[position] for position in positions]) 
 
 #----------------------------------------------------------------------------------
 
 def mark_position(position, marker, maze):
 	""" Mark the position in the maze with a marker type
-		Inputs:
-			position (row,col)
-			marker route|deadend
-			maze
 	"""
 	if maze[position] is not start:
 		maze[position] = marker
-		if marker == route:
-			print "Mark as explored route point"
-		else:
-			print "Mark as deadend"
-	else:
-		print "At the start"
 
 	return maze
 
 #----------------------------------------------------------------------------------
 
 def solve(position, maze):
-	print "current postion (%d,%d)" % position
 	
-	print "Is position in maze?"	
 	if position not in maze.keys():
-		print "No"
 		return False
-	print "Yes"
 
-	print "Is position the goal?"
 	if maze[position] is end:
-		print "Yes"
-		show_maze(maze)
 		return True
-	print "No"
 
-	print "Is position unexplored and on the path?"
 	if maze[position] not in [new,start]:
-		print "No"
 		return False
-	print "Yes"
 	
-	maze = mark_position(position, route, maze)
-	show_maze(maze)
+	maze = mark_position(position, been, maze)
 
 	if solve(right(position), maze) or solve(down(position), maze) \
 	or solve(left(position), maze) or solve(up(position), maze):
+		# marks all the way back along the successful branch
+		maze = mark_position(position, route, maze)
 		return True
 
-	print "backtracking ...."	
-	maze = mark_position(position, deadend, maze)
-	show_maze(maze)
 	return False
 	 
 	
@@ -136,8 +91,8 @@ def main():
 	args = parser.parse_args()
 	maze = parse_maze(args.file.readlines())
 
-	start = find_start(maze)
-	solve(start, maze)
+	solve(find_start(maze), maze)
+	show_maze(maze)
 
 #---------------------------------------------------------------------------------
 
